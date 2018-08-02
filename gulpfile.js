7670e2317a9646754dddfadf51644f582cdc1156
Jonathan Sexton <joncsexton@.com>
2018-08-02 15:34:21 -0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,6 @@ const gulp = require("gulp");
 const browserSync = require('browser-sync').create();
 const reload = browserSync.reload;
 
-// gulp.task("jonsTask", function() {
-//   console.log("*********************");
-//   console.log("jonsTask");
-//   console.log("*********************");
-// });
-
-// gulp.task("default", () => console.log("Arrow functions work in Gulp tasks?"));
-
-// browserSync.init({
-//   server: "./"
-// });
-// browserSync.stream();
-
 gulp.task('browser-sync', function() {
   browserSync.init({
      server: {
